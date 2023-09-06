d1707754320b17ed9b50a68a7d482625abcdb214
Venkat01890 <veera.srikakolapu18@gmail.com>
2023-09-06 14:24:10 +0530

    installing mysql and postfix

--- 09-functions.sh
+++ 09-functions.sh
@@ -4,12 +4,16 @@ DATE=$(date +%F)
 SCRIPT_NAME=$0
 LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
 
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
 VALIDATE(){
     if [ $1 -ne 0 ]
     then 
-        echo "$2 is error"
+        echo -e "$2 is $R FAILURE $N"
     else
-        echo "$2 is success"
+        echo -e "$2 is $G SUCCESS $N"
     fi
 }
 
