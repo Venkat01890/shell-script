249880773b8739837cd85e7e5fc3af3339d3553c
Venkat01890 <veera.srikakolapu18@gmail.com>
2023-09-08 11:49:58 +0530

    script added

--- 13-delete-old-logs.sh
+++ 13-delete-old-logs.sh
@@ -6,7 +6,7 @@ DATE=$(date +%F:%H:%M:%S)
 LOGSDIR=/home/centos/shellscript-logs
 # /home/centos/shellscript-logs/script-name-date.log
 SCRIPT_NAME=$0
-LOGFILE=$LOGSDIR/$0-$DATE.log
+LOGFILE=$LOGSDIR/$SCRIPT_NAME-$DATE.log
 
 FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +5)
 
