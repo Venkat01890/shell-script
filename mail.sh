612bba1b18da99ce8c1f29c433f9c3a1adae0751
Venkat01890 <veera.srikakolapu18@gmail.com>
2023-09-08 13:40:10 +0530

    script added

--- mail.sh
+++ mail.sh
@@ -4,8 +4,12 @@
 
 TO_ADDRESS=$1
 SUBJECT=$2
-BODY=$3
+BODY=$(sed -e 's/[]\/$*.^[]/\\&/g' <<< $3)
+echo "escaped content: $BODY"
 TEAM_NAME=$4
 ALERT_NAME=$5
 
-echo "all args: $@"
+#echo "all args: $@"
+
+FINAL_BODY=$(sed -e "s/TEAM_NAME/$TEAM_NAME/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/MESSAGE/$BODY/g" template.html)
+echo "$FINAL_BODY" | mail -s "$SUBJECT" $TO_ADDRESS
